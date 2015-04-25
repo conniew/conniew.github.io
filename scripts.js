8459fa4621a25c7892c1c7aa3341a45df59f8af1
Connie Wan <echorium@gmail.com>
2015-04-25 10:50:41 -0700

    Added rigging for control buttons, prev and next always disabled for now.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,34 @@
+var expandControl = document.querySelector('.control.expand');
+var collapseControl = document.querySelector('.control.collapse');
+
+function setControls() {
+  document.querySelector('#controls').style.width =
+      document.querySelector('#content').clientWidth + 'px';
+
+  // TODO: update this based on page, probably requires page# metadata
+  document.querySelector('.control.prev').disabled = true;
+  document.querySelector('.control.next').disabled = true;
+}
+
+function updateControls() {
+  expandControl.disabled = !document.querySelector('.preview:not(.active)');
+  collapseControl.disabled = !document.querySelector('.preview.active');
+}
+
+function expandAll() {
+  var collapsedPreviews = document.querySelectorAll('.preview:not(.active)');
+  for (var i = 0; i < collapsedPreviews.length; i++) {
+    togglePreview(collapsedPreviews[i], false);
+  }
+}
+
+function collapseAll() {
+  var expandedPreviews = document.querySelectorAll('.preview.active');
+  for (var i = 0; i < expandedPreviews.length; i++) {
+    togglePreview(expandedPreviews[i], false);
+  }
+}
+
 function smoothScroll(container, y1, y2, t) {
   if (t > 30) return;
   container.scrollTop = y1 + t*(y2 - y1)/30;
@@ -15,19 +46,23 @@ function scrollToElement(target) {
   smoothScroll(container, startY, endY, 0);
 }
 
-function expandPreview(preview) {
-  if (!preview.classList.contains('active'))
+function togglePreview(preview, scroll) {
+  if (!preview.classList.contains('active') && scroll)
     scrollToElement(preview);
   preview.classList.toggle('active');
   preview.querySelector('.body').style.top =
     preview.querySelector('.heading').offsetHeight + 'px';
+  updateControls();
 }
 
 function rigPreviews() {
   var headings = document.querySelectorAll('.preview .heading');
   for (var i = 0; i < headings.length; i++) {
-    headings[i].onclick = function(event) { expandPreview(event.target.parentNode); };
+    headings[i].onclick = function(event) { togglePreview(event.target.parentNode, true); };
   }
 }
 
+
 rigPreviews();
+setControls();
+updateControls();
